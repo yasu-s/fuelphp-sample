a1de530f20d774e5f77a2b228ffba1dbc3420a8b
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-04-05 01:22:12 +0000

    #1 ファイル配置

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fuelphp-sample
-FuelPHP sampl
+
+## 概要
+
+FuelPHP のサンプルソース
+
+## 実行
+
+- `php -S 0.0.0.0:8000`
+
+## 参考URL
+
+- https://fuelphp.com/
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP</title>
+    </head>
+    <body>
+        <?php
+            phpinfo();
+        ?>
+    </body>
+</html>
